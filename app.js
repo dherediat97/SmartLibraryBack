bf7a72d5c6fe45279f9c3cbb0fa3d40e390d156b
dherediat <dherediat97@gmail.com>
2024-12-11 21:19:30 +0100

    feature: add jwt authorization into api server

--- app.js
+++ app.js
@@ -1,57 +1,82 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
+const jwt = require('jsonwebtoken');
 
 var booksRouter = require('./routes/books');
 var bookRouter = require('./routes/book');
 var reviewsRouter = require('./routes/reviews');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 var corsOptions = {
   origin: 'http://localhost:5173',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+const secretKey = process.env.SECRET_KEY;
+
+function verifyToken(req, res, next) {
+  const header = req.header('Authorization') || '';
+  const token = header.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Access not authorizated' });
+  }
+  try {
+    const payload = jwt.verify(token, secretKey);
+    req.username = payload.username;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Token not valid' });
+  }
+}
+
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  try {
+    res.header('Access-Control-Allow-Origin', '*');
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Username and password are required' });
+    }
+    if (
+      username === process.env.API_USERNAME &&
+      password === process.env.API_PASSWORD
+    ) {
+      const token = jwt.sign({ username }, secretKey, { expiresIn: '3h' });
+      return res.status(200).json({ token });
+    } else {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 const port = 8080 | process.env.PORT;
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/', verifyToken, (req, res) => {
   res.json({ message: 'ok' });
 });
 
-app.use('/books', booksRouter);
-app.use('/book', bookRouter);
-app.use('/reviews', reviewsRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use('/books', verifyToken, booksRouter);
+app.use('/book', verifyToken, bookRouter);
+app.use('/reviews', verifyToken, reviewsRouter);
 
 app.listen(port, () => {});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
